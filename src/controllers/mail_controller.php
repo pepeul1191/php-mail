83b5ed18e13c988e62977a18f39ae70364cf6710
Pepe Valdivia <jvaldivia@softweb.pe>
2018-10-22 15:34:11 -0500

    Prueba de correo con string format en hosting

--- src/controllers/mail_controller.php
+++ src/controllers/mail_controller.php
@@ -26,7 +26,6 @@ class MailController extends \Configs\Controller
       //
       $to      = 'info@softweb.pe';
       $subject = 'Correo de Prueba';
-      $message = 'Hola</br>Att. Tu Zulo.';
       // To send HTML mail, the Content-type header must be set
       $headers  = 'MIME-Version: 1.0' . "\r\n";
       $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
