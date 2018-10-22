bd67ae0cb7889921ba62b7c6eb798b168f85b1a8
Pepe Valdivia <jvaldivia@softweb.pe>
2018-10-22 12:50:42 -0500

    Eliminando archivos no necesarios

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $settings = require __DIR__ . '/src/configs/settings.php';
 $app = new \Slim\App($settings);
 
 // Register database
-require __DIR__ . '/src/configs/database.php';
+//require __DIR__ . '/src/configs/database.php';
 // Set up dependencies
 require __DIR__ . '/src/configs/dependencies.php';
 // Register middleware
